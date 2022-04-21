d18b784c9b76735806c72983501276a9d9fefe5a
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2022-04-21 00:56:11 -0500

    Export module components
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2022 Intelica. Scientific and Software Solutions
+ * Author: Reinier Millo Sánchez <millo@intelica.mx>
+ *
+ * This file is part of the @intelica/odoo-xmlrpc package.
+ * This project is distributed under MIT License.
+ * Check LICENSE file in project root folder.
+ */
+
+/* Export api controllers*/
+export { Odoo } from "./controllers/odoo";
+export { OdooCRUD } from "./controllers/odoo.crud";
+export { MODEL_TYPE, IServerVersion, IContact, ILead, IEmployee } from "./models/odoo.types";
+
+/* Export api controller for Odoo models */
+export { Contacts } from "./controllers/contacts.odoo";
+export { Leads } from "./controllers/leads.odoo";
+export { Employees } from "./controllers/employees.odoo";
+
+/* Export models */
+export { ERRORS } from "./constants/errors.enum";
