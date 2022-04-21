ecb01a25541657c280d91af0bdb240d163d291cf
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2022-04-21 00:55:05 -0500

    Add base error definition
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

---
+++ src/constants/errors.enum.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) 2022 Intelica. Scientific and Software Solutions
+ * Author: Reinier Millo Sánchez <millo@intelica.mx>
+ *
+ * This file is part of the @intelica/odoo-xmlrpc package.
+ * This project is distributed under MIT License.
+ * Check LICENSE file in project root folder.
+ */
+
+/**
+ * Predefined errors
+ */
+export const ERRORS = {
+  INVALID_CREDENTIALS: {
+    value: 1001,
+    str: "odoo-invalid-credentials"
+  }
+};
